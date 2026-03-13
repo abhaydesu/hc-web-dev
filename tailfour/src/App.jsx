4b4f9623808d3b6d6e25dc481f35ba194f052837
Abhay <sketchologistabhay@gmail.com>
2026-03-13 21:49:02 +0530

    tailwind and grid

---
+++ tailfour/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+    <h1 className='kbd'>k</h1>
+    </div>
+  )
+}
+
+export default App
